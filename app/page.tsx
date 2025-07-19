5ff5f0de60060fe944f5e5609391eaeece374d3b
Kaleb-Besufikad <se.kaleb.besufikad@gmail.com>
2025-07-19 19:49:28 +0300

    feat: implement 'Home' Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,51 @@
-import React from 'react'
-import {Heading1} from "lucide-react";
-import {Button} from "@/components/ui/button";
+import React from "react";
+import CompanionCard from "@/components/CompanionCard";
+import CTA from "@/components/CTA";
+import CompanionsList from "@/components/CompanionsList";
+import { recentSessions } from "@/constants";
+import Image from "next/image";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to Converso</h1>
-      <Button>Let&#39;s Get started</Button>
-    </div>
+    <main>
+      <h1 className="text-2xl underline">Popular Companions</h1>
+      <section className="home-section">
+        <CompanionCard
+          id="123"
+          topic={"Neural Network of the Brain"}
+          name={"Neura the Brainy Explorer"}
+          duration={45}
+          subject={"science"}
+          color={"#ffda6e"}
+        />
+        <CompanionCard
+          id="456"
+          name={"Countsy the Number Wizard"}
+          topic={"Derivatives & Integrals"}
+          duration={45}
+          subject={"math"}
+          color={"#e5d0ff"}
+        />
+        <CompanionCard
+          id="789"
+          name={"Verba the Vocab builder"}
+          topic={"language"}
+          duration={45}
+          subject={"English Literature"}
+          color={"#BDE7FF"}
+        />
+      </section>
 
-  )
-}
+      <section className="home-section">
+        <CompanionsList
+          title={"Recent sessions"}
+          companions={recentSessions}
+          className={"w-2/3 max-lg:w-full"}
+        />
+        <CTA />
+      </section>
+    </main>
+  );
+};
 
-export default Page
+export default Page;
