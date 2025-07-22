04109208886e127496d6ef6a2375e43b492bfcd4
Kaleb-Besufikad <se.kaleb.besufikad@gmail.com>
2025-07-22 20:19:53 +0300

    feat: implement a companions page with filter and search features

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,6 +1,4 @@
 const CompanionSession = () => {
-	return (
-		<div>CompanionSession</div>
-	)
-}
-export default CompanionSession
+  return <div>CompanionSession</div>;
+};
+export default CompanionSession;
