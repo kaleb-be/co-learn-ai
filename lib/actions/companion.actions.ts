3d6e42fe330213ad5c5753dfec2a9be50d2b3cb0
Kaleb-Besufikad <se.kaleb.besufikad@gmail.com>
2025-08-14 19:32:05 +0300

    feat: implement profile on "/my-journey"

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -90,3 +90,14 @@ export const getUserSessions = async (userId: string, limit = 10) => {
   if (error) throw new Error(error.message);
   return data.map(({ companions }) => companions);
 };
+
+export const getUserCompanions = async (userId: string) => {
+  const supabase = createSupabaseClient();
+  const { data, error } = await supabase
+    .from("companions")
+    .select()
+    .eq("author", userId);
+  if (error) throw new Error(error.message);
+  return data;
+};
+
